03abbad591de6ddde2f808a2340ef384c327d36e
tad27 <tadesseakele27@gmail.com>
2023-04-28 03:16:09 -0700

    Inital Setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="dark:text-white text-3xl">Redux Todo</h1>
+    </div>
+  );
+}
+
+export default App;
